bb8e756d4742b7849736545840beb351cfa39e97
Zabith Fulail <muhammadh_s@epiclanka.net>
2024-03-12 08:02:11 +0530

    minor changes

---
+++ frontend/src/pages/login/login.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import '../login.css'
+const login = () => {
+    return (
+        <div>
+            <div className="Container">
+                <div className="header">
+                    <div className="text">Login</div>
+                    <div className="underline"></div>
+                </div>
+                <div className="inputs">
+                    <div className="input">
+                        <img src="" alt="" />
+                        <input type="text" />
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default login
